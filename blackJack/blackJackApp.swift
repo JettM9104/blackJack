e528d7edf29348134c20d93d27db12c207ccab08
JMu <64600882+JettM9104@users.noreply.github.com>
2025-06-04 09:39:38 -0400

    Initial Commit

---
+++ blackJack/blackJackApp.swift
@@ -0,0 +1,17 @@
+//
+//  blackJackApp.swift
+//  blackJack
+//
+//  Created by Jett Mu on 2025-06-04.
+//
+
+import SwiftUI
+
+@main
+struct blackJackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ blackJackTests/blackJackTests.swift
@@ -0,0 +1,17 @@
+//
+//  blackJackTests.swift
+//  blackJackTests
+//
+//  Created by Jett Mu on 2025-06-04.
+//
+
+import Testing
+@testable import blackJack
+
+struct blackJackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
